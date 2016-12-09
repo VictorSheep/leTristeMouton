114ebbb4b9772927544feee5e0a2fc2f73c37c1d
VictorSheep <victor.moutton@gmail.com>
2016-12-09 19:18:58 +0100

    Affichage des articles sur home.php

--- src/controllers/home_controller.php
+++ src/controllers/home_controller.php
@@ -1,6 +1,9 @@
 <?php 
 	function home_controller(){
-		$list_articles = get_recent_articles(3);
+		$nb_article = 8;
+		$nb_col = 4;
+		$ratio = $nb_article/$nb_col;
+		$list_articles = get_recent_articles($nb_article);
 		/*echo'<pre>';
 		print_r($list_articles);
 		echo'</pre>';*/
--- views/home.php
+++ views/home.php
@@ -1,12 +1,15 @@
 <?php ob_start() ; ?>
-	<div class="grid-3 has-gutter-xl home">
-	<?php foreach ($list_articles as $article): ?>
+	<div class="grid has-gutter-xl home">
+	<?php for ($i=0; $i < count($list_articles)/($ratio); $i++): ?>
 		<div>
-		<?php for ($i=0; $i < 4; $i++): ?>
+		<?php for ($j=0; $j < $ratio; $j++): ?>
+		<?php $key = $j*$nb_col+$i; ?>
+		<?php echo $key; ?>
+		<?php $article = $list_articles[$key] ?>
 			<div class="post-preview justify mb-4">
 				<div class="content mb-5 mside-4">
 					<a href="#">
-						<h4 class="mt-5"><?php echo $article['title'] ?></h4>
+						<h4 class="mt-5"><?php echo $article['id'].$article['title'] ?></h4>
 					</a>
 					<p> <?php echo $article['content'] ?></p>
 				</div>
@@ -16,7 +19,7 @@
 			</div>
 		<?php endfor ?>
 		</div>
-	<?php endforeach ?>
+	<?php endfor ?>
 	</div>
 <?php $content = ob_get_clean() ; ?>
 <?php include 'master.php' ?>
