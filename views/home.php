94626b17689462e266971304a81b216fa923dbb7
VictorSheep <victor.moutton@gmail.com>
2016-12-10 14:31:01 +0100

    Ajout de la date sur les preview d'article dans home.php

--- views/home.php
+++ views/home.php
@@ -11,9 +11,10 @@
 			<div class="post-preview justify mb-4">
 				<div class="content mb-5 mside-4">
 					<a href="#">
-						<h4 class="mt-5"><?php echo $article['title'] ?></h4>
+						<h4 class="mt-5"><?php echo $article['title']; ?></h4>
 					</a>
-					<p> <?php echo $article['content'] ?></p>
+					<p> <?php echo $article['content']; ?></p>
+					<p class="date"> <?php echo $article['date']; ?> </p>
 				</div>
 				<div class="center">
 					<a class="button-bot center" href="news">Lire la suite</a>
