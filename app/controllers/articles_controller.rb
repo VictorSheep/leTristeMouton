80834599259e1c17d82631ff7297cd5da8913cf7
Eclypso <tristan_belin@htmail.fr>
2017-05-06 10:23:04 +0200

    Gestion affichage des articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
-  def index
+  def home
+    @nb_articles = 9
+    @nb_col = 4
+    @ratio = @nb_articles / @nb_col
     @articles = Article.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+article1 = Article.create(title: "First", content: "Lorem Ipsum")
+article2 = Article.create(title: "Second", content: "Lorem Ipsum")
+article3 = Article.create(title: "Third", content: "Lorem Ipsum")
+article4 = Article.create(title: "Fourth", content: "Lorem Ipsum")
+article5 = Article.create(title: "Fifth", content: "Lorem Ipsum")
+article6 = Article.create(title: "Sixth", content: "Lorem Ipsum")
